fcb5b35ff4cdbf790468696de7beda371ded7133
Rodrigo <rodrigobrochardt@gmail.com>
2021-02-09 17:28:02 -0300

    create object note

---
+++ lib/models/note.dart
@@ -0,0 +1,27 @@
+class Note {
+  String title;
+  String description;
+  String employee;
+  String id;
+
+  Note({
+    this.title,
+    this.description,
+    this.employee,
+    this.id,
+  });
+  Note.fromJson(Map<String, dynamic> json, uid) {
+    title = json['title'];
+    description = json['description'];
+    employee = json['employee'];
+    id = uid;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['description'] = this.description;
+    data['employee'] = this.employee;
+    return data;
+  }
+}
