836da076350278d5b3c4587b084423f8c19eef2f
Rodrigo <rodrigobrochardt@gmail.com>
2021-02-09 01:16:22 -0300

    add fromJson and toJson to sale object

--- lib/models/sale.dart
+++ lib/models/sale.dart
@@ -3,13 +3,37 @@ class Sale {
   String employee;
   String date;
   String product;
-  int value;
-  int quantityProduct;
-  Sale(
-      {this.employee,
-      this.product,
-      this.date,
-      this.value,
-      this.quantityProduct,
-      this.id});
+  String value;
+  String productAmount;
+  String refUID;
+  Sale({
+    this.employee,
+    this.product,
+    this.date,
+    this.value,
+    this.productAmount,
+    this.id,
+    this.refUID,
+  });
+
+  Sale.fromJson(Map<String, dynamic> json, uid) {
+    employee = json['employee'];
+    value = json['value'];
+    refUID = json['refUID'];
+    id = uid;
+    productAmount = json['productAmount'];
+    date = json['date'];
+    product = json['product'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['product'] = this.product;
+    data['value'] = this.value;
+    data['refUID'] = this.refUID;
+    data['productAmount'] = this.productAmount;
+    data['date'] = this.date;
+    data['employee'] = this.employee;
+    return data;
+  }
 }
