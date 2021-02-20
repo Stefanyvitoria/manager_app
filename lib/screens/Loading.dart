8906f9b3002547ed203d2d09d05030bda93eb69d
Rodrigo <rodrigobrochardt@gmail.com>
2021-02-20 16:38:57 -0300

    fix theme on loading page

--- lib/screens/Loading.dart
+++ lib/screens/Loading.dart
@@ -1,26 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:manager_app/services/theme.dart';
+import 'package:provider/provider.dart';
 
 class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Themebuilder(
-      builder: (context, _themedata) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: _themedata,
-          home: Scaffold(
-            appBar: AppBar(
-              title: Text('Loading'),
-            ),
-            body: Center(
-              child: CircularProgressIndicator(
-                backgroundColor: Colors.teal,
+    return ChangeNotifierProvider(
+      create: (_) => ThemeNotifier(),
+      child: Consumer<ThemeNotifier>(
+        builder: (context, ThemeNotifier notifier, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: notifier.darkTheme ? dark : light,
+            home: Scaffold(
+              appBar: AppBar(
+                title: Text('Loading'),
+              ),
+              body: Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.teal,
+                ),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
